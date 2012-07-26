4dd3ea6135dd30229614ac3ed1a834d0ae221552
Dat Nguyen <nguyenchanhdat@gmail.com>
2012-07-26 10:43:32 +0200

    Taupunktberechnung-Funktion hinfügen

--- BASE.c
+++ BASE.c
@@ -8,11 +8,11 @@
 char cArray[10]; 
 int ic=0;
 
-double temp = 0;
+double temp = 0;//Messwerte von BMP085
+double temp2 = 0;//Messwerte von DHT22 
 double qfe = 0;
 double qff = 0;
 double feuchte = 0;
-double taupunkt = 0;
 
 //Variablen für QFF-Brechnung
 double x;
@@ -41,14 +41,32 @@ void setup(){
 void loop(){
   if (dataLesen()==1) {
   //QFF-Berechnung
-  x = ((temp + konst1)/(qfe/100)*exp(((temp+konst1+konst2)*11.5526-26821)/(temp+konst1+konst2-1060));
+  x = ((temp + konst1)/(qfe/100)*exp(((temp+konst1+konst2)*11.5526-26821)/(temp+konst1+konst2-1060)));
   qff = (qfe/100)*exp(konst2*10.5152/(x+temp+konst1+konst2));
+  
+    
+//Taupunkt berechnen
+  if (temp >= 0) {
+    a = 7.5;
+    b = 237.3;
+  } else {
+    a = 7.6;
+    b = 240.7;
+  }
+  
+  double SDD = 6.1078 * pow(10,((a*temp)/(b+temp))); // Sättigungsdampfdruck in hPa
+  double DD = feuchte/100 * SDD; //Dampfdruck in hPa
+  double v = log(DD/6.1078) / log(10);
+  double taupunkt = b * v /(a - v); //Taupunkttemperatur in °C
+  
   //Messwerten abschicken
   if (client.connect(serverName, 80)) {
     //Serial.println("Verbunden ... sende ... fertig!");
     // URL anrufen:
     client.print("GET /upload.php?TEMP=");
     client.print(temp);
+    client.print("&TEMP2=");
+    client.print(temp2);
     client.print("&TAU=");
     client.print(taupunkt);
     client.print("&QFE=");
@@ -87,7 +105,7 @@ int dataLesen() {
     incomingByte = Serial.read();
     
     if (incomingByte == 'T') temp = atof(cArray);
-    if (incomingByte == 'P') taupunkt = atof(cArray);
+    if (incomingByte == 'C') temp2 = atof(cArray);
     if (incomingByte == 'D') qfe = atof(cArray);
     if (incomingByte == 'F') {feuchte = atof(cArray); done = 1;}
     
