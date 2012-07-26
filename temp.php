0409b72d1c4b1e2b6cebd0de34b243fec05e0ef7
Dat Nguyen <nguyenchanhdat@gmail.com>
2012-07-26 10:48:43 +0200

    Minor

--- temp.php
+++ temp.php
@@ -4,7 +4,11 @@ include("db.php");
 
 $sqlTemp = "SELECT `DATUM`, `TEMP` FROM `1Tag` ORDER BY `ID` DESC LIMIT 6"; 
 $temp = mysql_query($sqlTemp) or die(mysql_error());
+$sqlTemp2 = "SELECT `DATUM`, `TEMP2` FROM `1Tag` ORDER BY `ID` DESC LIMIT 6"; 
+$temp2 = mysql_query($sqlTemp2) or die(mysql_error());
 echo "Temperaturverlauf der letzten 3 Stunden (Zeitintervall zwischen zwei Messungen ist 30 Minuten):";
+echo "<br />";
+echo "Sensor 1:";
 echo "<br />";
 
 while ($test = mysql_fetch_array($temp)) {
@@ -12,4 +16,10 @@ while ($test = mysql_fetch_array($temp)) {
 	echo "<br />";
 }
 
+echo "Sensor 2:";
+echo "<br />";
+
+while ($test2 = mysql_fetch_array($temp2)) {
+	echo $test['DATUM']." - ".$test['TEMP2']." &#176C";
+	echo "<br />";
 ?>
